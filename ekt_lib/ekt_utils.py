b6859b8d8785f13d5fd94f80f73d2409ac7cdc82
82D707A6DC90BF87F91BE51A692DAAF8 <82D707A6DC90BF87F91BE51A692DAAF8@i-search.com.cn>
2020-05-30 16:07:19 +0800

    push code

--- ekt_lib/ekt_utils.py
+++ ekt_lib/ekt_utils.py
@@ -211,6 +211,16 @@ def dvbs_symbol_err_rate_json_to_csv(json_path, csv_path):
     pd_data.to_csv(csv_path, index=None)
 
 
+def dvbs_symbol_rate_step_json_to_csv(json_path, csv_path):
+    load_dict = read_json_file(json_path)
+    list_data = load_dict.get("test_parame_result")
+    list_required_data = []
+    for i in list_data:
+        list_required_data.append([i[0][0], i[0][1], i[1]])
+    pd_data = pd.DataFrame(list_required_data, columns=['sfu_symbol_rate', 'stb_symbol_rate', 'test_result'])
+    pd_data.to_csv(csv_path, index=None)
+
+
 def dvbs2_dynamic_min_json_to_csv(json_path, csv_path):
     load_dict = read_json_file(json_path)
     list_data = load_dict.get("test_parame_result")
--- ekt_testcases/dvbs/dvbs_symbol_rate_step.py
+++ ekt_testcases/dvbs/dvbs_symbol_rate_step.py
@@ -6,10 +6,11 @@ import json
 import datetime
 from ekt_lib import ekt_net, ekt_cfg
 from ekt_lib.ekt_sfe import Ektsfe
+from pathlib2 import Path
 from ekt_lib.ekt_stb_tester import stb_tester_execute_testcase
 from ekt_lib.threshold_algorithm_SFE import mosaic_algorithm
-from ekt_lib.ekt_utils import write_test_result, read_ekt_config_data, generate_symbol_rate_list, \
-    find_level_offset_by_frequency
+from ekt_lib.ekt_utils import write_test_result, find_level_offset_by_frequency, write_json_file, read_json_file, \
+    dvbs_symbol_rate_step_json_to_csv, generate_symbol_rate_list
 
 CODE_RATE_3_4 = "R3_4"
 MODULATION_QPSK = "S4"
@@ -18,6 +19,17 @@ LEVEL_OFFSET = find_level_offset_by_frequency("DVBS_S2_FREQUENCY_LEVEL_OFFSET", 
 LEVEL_70 = str("%.2f" % ((-70) - LEVEL_OFFSET))
 
 SYMBOL_RATE_LIST = generate_symbol_rate_list()
+
+my_file = Path("../../ekt_json/dvbs_symbol_rate_step.json")
+if my_file.exists():
+    pass
+else:
+    dict_test_parame_result = {}
+    list_test_parame_result = []
+    for SYMBOL_RATE in SYMBOL_RATE_LIST:
+        list_test_parame_result.append([SYMBOL_RATE, None])
+    dict_test_parame_result["test_parame_result"] = list_test_parame_result
+    write_json_file("../../ekt_json/dvbs_symbol_rate_step.json", dict_test_parame_result)
 
 if __name__ == '__main__':
     """
@@ -30,25 +42,17 @@ if __name__ == '__main__':
     是否需要对testcase与PC端做参数交互？）
     ⑤依次修改可变参数，判断机顶盒画面是否含有马赛克并记录结果
     """
+    load_dict = read_json_file("../../ekt_json/dvbs_symbol_rate_step.json")
     sfe_ip = "192.168.1.47"
     specan = Ektsfe(sfe_ip)
     specan.clean_reset()
     specan = Ektsfe(sfe_ip)
     specan.preset_instrument()
-    # specan.timeout = 2000
     specan = Ektsfe(sfe_ip)
     specan.set_digitaltv_input_source("TSPL")
     specan = Ektsfe(sfe_ip)
     specan.set_digitaltv_input_load(r"D:\TSGEN\SDTV\DVB_25Hz\720_576i\LIVE\DIVER.GTS")
 
-    dict_data = read_ekt_config_data("../../ekt_lib/ekt_config.json")
-    # DVBS_S2_FREQUENCY_LEVEL_OFFSET = dict_data.get("DVBS_S2_FREQUENCY_LEVEL_OFFSET")
-    # DVBS_QPSK_CODE_RATE_CN = dict_data.get("DVBS_QPSK_CODE_RATE_CN")
-
-    # DVBS2_8PSK_CODE_RATE_CN = dict_data.get("DVBS2_8PSK_CODE_RATE_CN")
-
-    # for code_rate_cn in DVBS_QPSK_CODE_RATE_CN:
-    #     del specan
     specan = Ektsfe(sfe_ip)
     specan.set_digitaltv_coding_constellation("S4")
     specan = Ektsfe(sfe_ip)
@@ -59,7 +63,14 @@ if __name__ == '__main__':
     # specan.set_frequency_frequency_frequency(FREQUENCY_1550)
     specan = Ektsfe(sfe_ip)
     specan.set_level_level_level(LEVEL_70 + " dBm")
-    for SYMBOL_RATE in SYMBOL_RATE_LIST:
+    # for SYMBOL_RATE in SYMBOL_RATE_LIST:
+    for PARAMETER in load_dict.get("test_parame_result"):
+        if PARAMETER[1] == None:
+            pass
+        else:
+            continue
+
+        SYMBOL_RATE = PARAMETER[0]
         specan = Ektsfe(sfe_ip)
         specan.set_digitaltv_coding_symbolrate(SYMBOL_RATE[0])
 
@@ -78,8 +89,8 @@ if __name__ == '__main__':
         触发stb-tester进行频率和符号率设置
         """
         stb_tester_execute_testcase(ekt_cfg.STB_TESTER_URL, ekt_cfg.BANCH_ID,
-                                 ["tests/front_end_test/testcases.py::test_continuous_button"],
-                                 "auto_front_end_test", "DSD4614iALM")
+                                    ["tests/front_end_test/testcases.py::test_continuous_button"],
+                                    "auto_front_end_test", "DSD4614iALM")
         net = ekt_net.EktNetClient('192.168.1.24', 9999)
         lock_state = net.send_rec(json.dumps({"cmd": "get_lock_state"}))
         if lock_state == "1":
@@ -94,29 +105,20 @@ if __name__ == '__main__':
         else:
             write_test_result("../../ekt_log/test_result_sfe.txt", ("出错了" + "\n"))
             continue
-        try:
-            start_data_result = mosaic_algorithm(sfe_ip, LEVEL_70, "-50")
-            # res = iterate_to_find_threshold(sfe_ip, -50, -100)
-            print "dvbs_symbol_rate_step: current_time:{}, coderate：{}, frequency：{} MHz，symbol_rate：{} Ksym/s，level：{} dbm, 马赛克检测结果：{}".format(
-                datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), CODE_RATE_3_4,
-                str(FREQUENCY_1550), str(SYMBOL_RATE[1]), LEVEL_70, start_data_result.get("detect_mosic_result"))
-            write_test_result("../../ekt_log/test_result_sfe.txt",
-                              "dvbs_symbol_rate_step: current_time:{}, coderate：{}, frequency：{} MHz，symbol_rate：{} Ksym/s，level：{} dbm, 马赛克检测结果：{}".format(
-                                  datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), CODE_RATE_3_4,
-                                  str(FREQUENCY_1550), str(SYMBOL_RATE[1]), LEVEL_70,
-                                  start_data_result.get("detect_mosic_result")) + "\n")
-        except:
-            start_data_result = mosaic_algorithm(sfe_ip, LEVEL_70, "-50")
-            print "dvbs_symbol_rate_step: current_time:{}, coderate：{}, frequency：{} MHz，symbol_rate：{} Ksym/s，level：{} dbm, 马赛克检测结果：{}".format(
-                datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), CODE_RATE_3_4,
-                str(FREQUENCY_1550), str(SYMBOL_RATE[1]), LEVEL_70, start_data_result.get("detect_mosic_result"))
-            write_test_result("../../ekt_log/test_result_sfe.txt",
-                              "dvbs_symbol_rate_step: current_time:{}, coderate：{}, frequency：{} MHz，symbol_rate：{} Ksym/s，level：{} dbm, 马赛克检测结果：{}".format(
-                                  datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), CODE_RATE_3_4,
-                                  str(FREQUENCY_1550), str(SYMBOL_RATE[1]), LEVEL_70,
-                                  start_data_result.get("detect_mosic_result")) + "\n")
 
-        """
-        进行机顶盒的频率修改或其他参数的修改
-        读取误码率或者判断机顶盒是否含有马赛克
-        """
+        start_data_result, mosaic_result = mosaic_algorithm(sfe_ip, LEVEL_70, "-50")
+        # res = iterate_to_find_threshold(sfe_ip, -50, -100)
+        print (
+            "dvbs_symbol_rate_step: current_time:{}, coderate：{}, frequency：{} MHz，symbol_rate：{} Ksym/s，level：{} dbm, 马赛克检测结果：{}".format(
+                datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), CODE_RATE_3_4,
+                str(FREQUENCY_1550), str(SYMBOL_RATE[1]), LEVEL_70, start_data_result.get("detect_mosic_result")))
+        write_test_result("../../ekt_log/test_result_sfe.txt",
+                          "dvbs_symbol_rate_step: current_time:{}, coderate：{}, frequency：{} MHz，symbol_rate：{} Ksym/s，level：{} dbm, 马赛克检测结果：{}".format(
+                              datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), CODE_RATE_3_4,
+                              str(FREQUENCY_1550), str(SYMBOL_RATE[1]), LEVEL_70,
+                              start_data_result.get("detect_mosic_result")) + "\n")
+
+        PARAMETER[1] = mosaic_result
+        write_json_file("../../ekt_json/dvbs_symbol_rate_step.json", load_dict)
+        dvbs_symbol_rate_step_json_to_csv("../../ekt_json/dvbs_symbol_rate_step.json",
+                                          "../../ekt_test_report/dvbs_symbol_rate_step.csv")
