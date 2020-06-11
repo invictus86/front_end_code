f1477fdd4017db8d62ff88b1235f7b1b7fac740c
82D707A6DC90BF87F91BE51A692DAAF8 <82D707A6DC90BF87F91BE51A692DAAF8@i-search.com.cn>
2020-06-11 16:16:49 +0800

    push code

--- ekt_lib/ekt_cfg.py
+++ ekt_lib/ekt_cfg.py
@@ -1,56 +1,56 @@
 # stb_tester config
-BANCH_ID = "1f7c848"
+BANCH_ID = "7750e4c"
 STB_TESTER_URL = "http://192.168.1.154"
 STB_TESTER_IP = "192.168.1.154"
 
 FRONT_END_SERVER_IP = "192.168.1.24"
 FRONT_END_SERVER_PORT = 9999
 
-DVB_S_LOCK_FUNCTION = ["tests/front_end_test/testcases.py::test_continuous_button"]
+DVB_S_LOCK_FUNCTION = ["tests/front_end_test/testcases.py::test_continuous_button_dsn5414a"]
 DVB_S_CATEGORY = "auto_front_end_test"
 DVB_S_REMOTE = "DSD4614iALM"
 
-DVB_S2_LOCK_FUNCTION = ["tests/front_end_test/testcases.py::test_continuous_button"]
+DVB_S2_LOCK_FUNCTION = ["tests/front_end_test/testcases.py::test_continuous_button_dsn5414a"]
 DVB_S2_CATEGORY = "auto_front_end_test"
 DVB_S2_REMOTE = "DSD4614iALM"
 
-DVB_T_LOCK_FUNCTION = ["tests/front_end_test/testcases.py::test_continuous_button_7414g"]
+DVB_T_LOCK_FUNCTION = ["tests/front_end_test/testcases.py::test_continuous_button_dtn7414g"]
 DVB_T_CATEGORY = "auto_front_end_test"
 DVB_T_REMOTE = "DSD4614iALM"
 
-DVB_T2_LOCK_FUNCTION = ["tests/front_end_test/testcases.py::test_continuous_button_7414g"]
+DVB_T2_LOCK_FUNCTION = ["tests/front_end_test/testcases.py::test_continuous_button_dtn7414g"]
 DVB_T2_CATEGORY = "auto_front_end_test"
 DVB_T2_REMOTE = "DSD4614iALM"
 
-DVB_C_LOCK_FUNCTION = ["tests/front_end_test/testcases.py::test_continuous_button_7514i"]
+DVB_C_LOCK_FUNCTION = ["tests/front_end_test/testcases.py::test_continuous_button_dcn7514i"]
 DVB_C_CATEGORY = "auto_front_end_test"
 DVB_C_REMOTE = "dcn7514i"
 
-DVB_J83_LOCK_FUNCTION = ["tests/front_end_test/testcases.py::test_continuous_button_7514i"]
+DVB_J83_LOCK_FUNCTION = ["tests/front_end_test/testcases.py::test_continuous_button_dcn7514i"]
 DVB_J83_CATEGORY = "auto_front_end_test"
 DVB_J83_REMOTE = "dcn7514i"
 
-DVB_T_SET_SEARCH_FUNCTION = ["tests/front_end_test/testcases.py::test_continuous_button_7414g_set_search"]
+DVB_T_SET_SEARCH_FUNCTION = ["tests/front_end_test/testcases.py::test_continuous_button_dtn7414g_set_search"]
 DVB_T_SET_SEARCH_CATEGORY = "auto_front_end_test"
 DVB_T_SET_SEARCH_REMOTE = "DSD4614iALM"
 
-DVB_T_SET_FREQUENCY_BANDWIDTH_FUNCTION = ["tests/front_end_test/testcases.py::test_continuous_button_7414g_set_frequency_bandwidth"]
+DVB_T_SET_FREQUENCY_BANDWIDTH_FUNCTION = ["tests/front_end_test/testcases.py::test_continuous_button_dtn7414g_set_frequency_bandwidth"]
 DVB_T_SET_FREQUENCY_BANDWIDTH_CATEGORY = "auto_front_end_test"
 DVB_T_SET_FREQUENCY_BANDWIDTH_REMOTE = "DSD4614iALM"
 
-DVB_T_OCR_FUNCTION = ["tests/front_end_test/testcases.py::test_ocr_strength_quality"]
+DVB_T_OCR_FUNCTION = ["tests/front_end_test/testcases.py::test_ocr_strength_quality_dtn7414g"]
 DVB_T_OCR_CATEGORY = "auto_front_end_test"
 DVB_T_OCR_REMOTE = "DSD4614iALM"
 
-DVB_T2_SET_SEARCH_FUNCTION = ["tests/front_end_test/testcases.py::test_continuous_button_7414g_set_search"]
+DVB_T2_SET_SEARCH_FUNCTION = ["tests/front_end_test/testcases.py::test_continuous_button_dtn7414g_set_search"]
 DVB_T2_SET_SEARCH_CATEGORY = "auto_front_end_test"
 DVB_T2_SET_SEARCH_REMOTE = "DSD4614iALM"
 
-DVB_T2_SET_FREQUENCY_BANDWIDTH_FUNCTION = ["tests/front_end_test/testcases.py::test_continuous_button_7414g_set_frequency_bandwidth"]
+DVB_T2_SET_FREQUENCY_BANDWIDTH_FUNCTION = ["tests/front_end_test/testcases.py::test_continuous_button_dtn7414g_set_frequency_bandwidth"]
 DVB_T2_SET_FREQUENCY_BANDWIDTH_CATEGORY = "auto_front_end_test"
 DVB_T2_SET_FREQUENCY_BANDWIDTH_REMOTE = "DSD4614iALM"
 
-DVB_T2_OCR_FUNCTION = ["tests/front_end_test/testcases.py::test_ocr_strength_quality"]
+DVB_T2_OCR_FUNCTION = ["tests/front_end_test/testcases.py::test_ocr_strength_quality_dtn7414g"]
 DVB_T2_OCR_CATEGORY = "auto_front_end_test"
 DVB_T2_OCR_REMOTE = "DSD4614iALM"
 
